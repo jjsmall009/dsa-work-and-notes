944471d54b380fe0fefd62c8c6a90911dfbaaba1
JJ Small <jjsmall009@gmail.com>
2022-01-07 14:52:03 -0800

    More array work

--- 01-arrays/NthSmallest.java
+++ 01-arrays/NthSmallest.java
@@ -2,6 +2,7 @@
 * Given an array and a value n (that's no bigger than the length of the array) find
 * the nth smallest element in the array.
 
+* Each array value <= 100,000
 * Time complexity = O(n)
 * Extra space = O(1)
 */
@@ -23,6 +24,30 @@ public class NthSmallest {
         Arrays.sort(arr);
 
         return arr[k - 1];
+    }
+
+    // Since each value in the array is unique and we know that maximum value of each element,
+    // we can use each value as an index in a new array and use extra space but get it done
+    // in O(n) time.
+    public static int IndexTheValues(int[] arr, int k) {
+        int[] sorted = new int[100000];
+        int result = -1;
+
+        for(int x: arr) {
+            sorted[x] = x;
+        }
+
+        int counter = 0;
+        for(int x: sorted) {
+            if (x != 0) {
+                counter += 1;
+                if (counter == k) {
+                    result = x;
+                    break;
+                }
+            }
+        }
+        return result;
     }
 
     public static int QuickPivotSortThing(int[] arr, int k) {
