d2db05795869a6ef29bf9d1ed1680c8716b5dbd0
JJ Small <jjsmall009@gmail.com>
2022-01-08 09:07:53 -0800

    Finished the basic LL implementation and working on problems now

--- 02-linked-lists/LinkedList.java
+++ 02-linked-lists/LinkedList.java
@@ -1,8 +1,9 @@
 /**
 * A basic implementation of a single linked list.
- */
+*/
 
 public class LinkedList{
+    // A node is just a piece of data and a pointer to the next node in the chain
     private class Node {
         private int data;
         private Node next;
@@ -13,25 +14,111 @@ public class LinkedList{
     }
 
     public Node head;
+    private int size;
 
+    // For simplicity the only kind of linked list has at least one element in it
     public LinkedList(int data) {
-        this.head = new Node(data);
+        this.head = new Node(data, null);
+        size += 1;
     }
 
     public void addFirst(int data) {
-
+        head = new Node(data, head);
+        size += 1;
     }
 
     public void addLast(int data) {
-        
+        Node p = head;
+
+        while(p.next != null) {
+            p = p.next;
+        }
+
+        p.next = new Node(data, null);
+        size += 1;
+    }
+
+    // Deletes the first occurance of the key, if found
+    public void deleteNode(int key) {
+        Node current = head;
+        Node previous = null;
+
+        // Edge case if the list has only 1 element
+        if (current.next == null) {
+            System.out.println("Cannot delete a node from a one element list");
+            return;
+        }
+
+        // Edge case for if the head of the list is the key
+        if (current.data == key && current.next != null) {
+            head = current.next;
+            return;
+        }
+
+        while (current != null) {
+            if (current.data == key) {
+                previous.next = current.next;
+                size -= 1;
+                return;
+            } else {
+                previous = current;
+                current = current.next;
+            }
+        }
+    }
+
+    // Returns the index of the first matching value, if found
+    public int searchList(int key) {
+        Node current = head;
+        int index = 0;
+
+        while(current != null) {
+            if (current.data == key) {
+                return index;
+            }
+            index += 1;
+            current = current.next;
+        }
+
+        return -1;
     }
 
     public void getHead() {
         System.out.println(this.head.data);
     }
 
+    public int getSize() {
+        return this.size;
+    }
+
+    public void printList() {
+        Node p = head;
+
+        while (p != null) {
+            System.out.print(p.data + " -> ");
+            p = p.next;
+        }
+        System.out.println();
+    }
+
+
     public static void main(String[] args) {
         myList = new LinkedList(12);
-        myList.getHead();
+        myList.addFirst(8);
+        myList.addFirst(2);
+        myList.addFirst(6);
+        myList.addLast(18);
+        myList.addLast(27);
+
+        myList.printList();
+        int size = myList.getSize();
+        System.out.println(size);
+
+        myList.deleteNode(12);
+        myList.printList();
+        int size1 = myList.getSize();
+        System.out.println(size1);
+
+        System.out.println(myList.searchList(18));
     }
 }
--- 02-linked-lists/linked-notes.md
+++ 02-linked-lists/linked-notes.md
@@ -1,3 +1,19 @@
 # Linked Lists - Notes
 
 Much like an array a linked list is a linear data strcuture but with some better (and some worse) features. A linked list is dynamic and you can grow or shrink it on the fly.
+
+## Linked List Basics
+
+* **Nodes** - A link list is comprised of nodes, and each node has a piece of data and a pointer to the next node in the list.
+* **Adding an element** - There are many ways to insert an element into the list, but the easy ways are either at the head or the tail. To insert at the head just make a new node and assign it to the head. As for inserting at the tail, loop through the list and once you get to the end add it.
+* **Deleting an element** - To delete an element (say, the first occurrence of a value), maintain two pointers as you iterate through the list. A pointer for the current node and a pointer for the previous node. Once you get to the node to delete just set the previous nodes pointer to the current nodes next pointer and voila, the node is gone.
+
+## Linked List Problems
+
+### > Detecting Loops
+
+### > Removing duplicates
+
+### > Reverse a Linked List
+
+### > Check if a list is a palindrome
