44bb9c46de0746569eb18c8afcd6dc3c89ba9a18
JJ Small <jjsmall009@gmail.com>
2022-01-12 10:23:17 -0800

    Iterative and recursive traversals have been implemented

--- 03-binary-search-trees/BinarySearchTree.java
+++ 03-binary-search-trees/BinarySearchTree.java
@@ -49,19 +49,78 @@ public class BinarySearchTree {
     // Inorder traversal visits the left subtree, then the current node, and
     // then vists the right subtree.
     public void inorderTraversal() {
-
-
+        Node current = this.root;
+        inorderHelper(current);
     }
 
-    // Preorder traversal visits the current node, the left subtree, and then the right subtree.
-    public void preorderTraversal() {
+    private void inorderHelper(Node current) {
+        if (current != null) {
+            inorderHelper(current.left);
+            System.out.print(current.value + " ");
+            inorderHelper(current.right);
+        }
+    }
 
+    public void iterativeInorder() {
+        Stack<Node> stack = new Stack<>();
+        Node current = this.root;
+
+        while(current != null || !stack.isEmpty()) {
+            // Go as far left as we can
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+
+            current = stack.pop();
+            System.out.print(current.value + " ");
+            current = current.right;
+        }
+    }
+
+    // Preorder visits the current node, then the left subtree, and finally the right subtree
+    public void preorderTraversal() {
+        Node current = this.root;
+        preorderHelper(current);
+    }
+
+    private void preorderHelper(Node current) {
+        if (current != null) {
+            System.out.print(current.value + " ");
+            preorderHelper(current.left);
+            preorderHelper(current.right);
+        }
+    }   
+
+    // Use a stack to do the stuff 
+    public void iterativePreorder() {
+        Stack<Node> stack = new Stack<>();
+
+        stack.push(this.root);
+        while(!stack.isEmpty()) {
+            Node current = stack.pop();
+
+            if (current != null) {
+                System.out.print(current.value + " ");
+                stack.push(current.right);
+                stack.push(current.left);
+            }
+        }
     }
 
     // Postorder visits the left, right, and then the current node.
     public void postorderTraversal() {
-
+        Node current = this.root;
+        postorderHelper(current);
     }
+
+    private void postorderHelper(Node current) {
+        if (current != null) {
+            postorderHelper(current.left);
+            postorderHelper(current.right);
+            System.out.print(current.value + " ");
+        }
+    }   
 
     public void getRoot() {
         System.out.println(root.value);
@@ -76,8 +135,15 @@ public class BinarySearchTree {
         tree.insertNode(7);
         tree.insertNode(15);
         tree.insertNode(18);
+        tree.insertNode(17);
         tree.insertNode(20);
 
         tree.getRoot();
+
+        tree.inorderTraversal();
+        System.out.println();
+        tree.preorderTraversal();
+        System.out.println();
+        tree.postorderTraversal();
     }
 }
